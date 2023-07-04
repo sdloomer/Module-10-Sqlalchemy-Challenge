cad5029f4d6694620563f95e4a9ed0e687b1a70a
Stephanie Loomer <sdloomer@gmail.com>
2023-07-04 15:57:36 -0700

    finished app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -18,8 +18,8 @@ Base = automap_base()
 Base.prepare(autoload_with=engine)
 
 # Save references to each table
-measurement = Base.classes.measurement
-station = Base.classes.station
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -37,6 +37,7 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return (
+        f"Welcome to the Climate API!</br>"
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation</br>"
         f"/api/v1.0/stations</br>"
@@ -48,9 +49,8 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
-    one_year = session.query(measurement.date, measurement.prcp).filter(measurement.date <= '2017-08-23', measurement.date >= '2016-08-23')
+    one_year = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date <= '2017-08-23', Measurement.date >= '2016-08-23')
     session.close()
-
     all_precipitation = []
     for date, prcp in one_year:
         results_dict = {}
@@ -62,9 +62,17 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     session = Session(engine)
-    results = session.query(station.station).all()
+    all_results = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
     session.close()
-    all_stations = list(np.ravel(results))
+    all_stations = []
+    for station, name, latitude, longitude, elevation in all_results:
+        station_dictionary = {}
+        station_dictionary['station'] = station
+        station_dictionary['name'] = name
+        station_dictionary['latitude'] = latitude
+        station_dictionary['longitude'] = longitude
+        station_dictionary['elevation'] = elevation
+        all_stations.append(station_dictionary)
     return jsonify(all_stations)
 
 @app.route("/api/v1.0/tobs")
@@ -76,29 +84,30 @@ def tobs():
     return jsonify(most_active)
 
 @app.route("/api/v1.0/<start>")
-def start(start):
+def start_temp(start):
     session = Session(engine)
-    start == session.query(measurement.date == start).all()
-    search_list = [func.min(measurement.tobs),
-                   func.max(measurement.tobs),
-                   func.avg(measurement.tobs)]
-    search_list_results = session.query(*search_list).filter(measurement.date == start).all()
+    results = session.query(func.min(Measurement.tobs), 
+                            func.max(Measurement.tobs), 
+                            func.avg(Measurement.tobs)).filter(Measurement.date >= start).all()
     session.close()
-    query_results = list(np.ravel(search_list_results))
-    return jsonify(query_results)
+    results_dictionary = {}
+    results_dictionary["min_temp"] = results[0][0]
+    results_dictionary["max_temp"] = results[0][1]
+    results_dictionary["avg_temp"] = results[0][2]
+    return jsonify(results_dictionary)
 
 @app.route("/api/v1.0/<start>/<end>")
-def start_end(start, end):
+def start_end_temp(start, end):
     session = Session(engine)
-    start = session.query(measurement.date >= start).all()
-    end = session.query(measurement.date <= end).all()
-    search_list = [func.min(measurement.tobs),
-                   func.max(measurement.tobs),
-                   func.avg(measurement.tobs)]
-    search_list_results = session.query(*search_list).filter(measurement.date == start).filter(measurement.date == end).all()
+    results = session.query(func.min(Measurement.tobs), 
+                            func.max(Measurement.tobs), 
+                            func.avg(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
     session.close()
-    query_results = list(np.ravel(search_list_results))
-    return jsonify(query_results)
+    results_dictionary = {}
+    results_dictionary["min_temp"] = results[0][0]
+    results_dictionary["max_temp"] = results[0][1]
+    results_dictionary["avg_temp"] = results[0][2]
+    return jsonify(results_dictionary)
 
 if __name__ == "__main__":
     app.run(debug=True)
